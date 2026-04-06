3b65b87e155308d752e86a5e6a8ffaaa5637e373
Bruno <bruno@example.com>
2026-04-06 10:32:55 -0300

    feat: add Cpf and Endereco fields to the Pessoa model via migration

---
+++ Migrations/20260406132914_AddEnderecoAndCpf.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace easydemandasapi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEnderecoAndCpf : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Cpf",
+                table: "Pessoas",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Endereco",
+                table: "Pessoas",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Cpf",
+                table: "Pessoas");
+
+            migrationBuilder.DropColumn(
+                name: "Endereco",
+                table: "Pessoas");
+        }
+    }
+}
